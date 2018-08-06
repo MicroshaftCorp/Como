c394cca5130eda628a6624f272cdf10c02216f66
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-06 11:06:28 -0700

    Create note.c

---
+++ note.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main( )
+{
+        printf("your *$FG%$!*ed by COMO Malware ItyIt!\n");
+        
+ 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+BitBlt(hdc, 0, 0, w, h, hdc, 0, 0, NOTSRCCOPY);
+	
+	out: return 100;
+}
+
+}
