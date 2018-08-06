06dfd21d9317868a04bb449b18de0c02a8f7390b
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-06 10:50:10 -0700

    Create comobrowse.bat

---
+++ comobrowse.bat
@@ -0,0 +1,10 @@
+@echo off
+title Web Browser
+:start
+timeout 1
+start https://www.google.com/search?safe=active&q=windows+xp+blue+screen+generator&sa=X&ved=0ahUKEwj1sIL0-9jcAhXExlkKHWfoBGEQ1QIIvQEoBA&biw=1366&bih=662
+timeout 1
+start https://www.click-test.com/
+timeout 1
+start http://atom.smasher.org/error/?icon=Error3&style=xp&title=Microsoft+Windows&url=&text=i+hate+you&b1=&b2=OK&b3=
+goto start
